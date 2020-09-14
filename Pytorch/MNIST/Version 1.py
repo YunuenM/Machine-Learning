500efeee33421b523d6ceca6a4c5affe349db4e0
Matthew <54723840+YunuenM@users.noreply.github.com>
2020-09-14 10:12:23 -0400

    Create Version 1.py

---
+++ Pytorch/MNIST/Version 1.py
@@ -0,0 +1,167 @@
+import torch
+import numpy as np
+from torch import optim, nn
+import torch.nn.functional as F
+from torchvision import datasets, transforms
+from torchvision.transforms import ToTensor
+import matplotlib.pyplot as plt
+from matplotlib.pyplot import imshow
+import matplotlib.image as mpimg 
+from google.colab.patches import cv2_imshow
+from PIL import Image
+%matplotlib inline
+
+from torch.autograd import Variable
+
+# This model is from the Udacity Course: Introduction to Deep Learning with Pytorch
+
+def test_network(net, trainloader):
+
+    criterion = nn.MSELoss()
+    optimizer = optim.Adam(net.parameters(), lr=0.001)
+
+    dataiter = iter(trainloader)
+    images, labels = dataiter.next()
+
+    # Create Variables for the inputs and targets
+    inputs = Variable(images)
+    targets = Variable(images)
+
+    # Clear the gradients from all Variables
+    optimizer.zero_grad()
+
+    # Forward pass, then backward pass, then update weights
+    output = net.forward(inputs)
+    loss = criterion(output, targets)
+    loss.backward()
+    optimizer.step()
+
+    return True
+
+
+def imshow(image, ax=None, title=None, normalize=True):
+    """Imshow for Tensor."""
+    if ax is None:
+        fig, ax = plt.subplots()
+    image = image.numpy().transpose((1, 2, 0))
+
+    if normalize:
+        mean = np.array([0.485, 0.456, 0.406])
+        std = np.array([0.229, 0.224, 0.225])
+        image = std * image + mean
+        image = np.clip(image, 0, 1)
+
+    ax.imshow(image)
+    ax.spines['top'].set_visible(False)
+    ax.spines['right'].set_visible(False)
+    ax.spines['left'].set_visible(False)
+    ax.spines['bottom'].set_visible(False)
+    ax.tick_params(axis='both', length=0)
+    ax.set_xticklabels('')
+    ax.set_yticklabels('')
+
+    return ax
+
+
+def view_recon(img, recon):
+    ''' Function for displaying an image (as a PyTorch Tensor) and its
+        reconstruction also a PyTorch Tensor
+    '''
+
+    fig, axes = plt.subplots(ncols=2, sharex=True, sharey=True)
+    axes[0].imshow(img.numpy().squeeze())
+    axes[1].imshow(recon.data.numpy().squeeze())
+    for ax in axes:
+        ax.axis('off')
+        ax.set_adjustable('box-forced')
+
+def view_classify(img, ps, version="MNIST"):
+    ''' Function for viewing an image and it's predicted classes.
+    '''
+    ps = ps.data.numpy().squeeze()
+
+    fig, (ax1, ax2) = plt.subplots(figsize=(6,9), ncols=2)
+    ax1.imshow(img.resize_(1, 28, 28).numpy().squeeze())
+    ax1.axis('off')
+    ax2.barh(np.arange(10), ps)
+    ax2.set_aspect(0.1)
+    ax2.set_yticks(np.arange(10))
+    if version == "MNIST":
+        ax2.set_yticklabels(np.arange(10))
+    elif version == "Fashion":
+        ax2.set_yticklabels(['T-shirt/top',
+                            'Trouser',
+                            'Pullover',
+                            'Dress',
+                            'Coat',
+                            'Sandal',
+                            'Shirt',
+                            'Sneaker',
+                            'Bag',
+                            'Ankle Boot'], size='small');
+    ax2.set_title('Class Probability')
+    ax2.set_xlim(0, 1.1)
+
+    plt.tight_layout()
+
+class Net(nn.Module):
+  def __init__(self):
+    super().__init__()
+    self.fc1 = nn.Linear(784, 256)
+    self.fc2 = nn.Linear(256, 128)
+    self.fc3 = nn.Linear(128, 64)
+    self.fc4 = nn.Linear(64, 10)
+    self.dropout = nn.Dropout(p=0.2)
+  def forward(self, x):
+    x = x.view(x.shape[0], -1)
+    x = self.dropout(F.relu(self.fc1(x)))
+    x = self.dropout(F.relu(self.fc2(x)))
+    x = self.dropout(F.relu(self.fc3(x)))
+    x = F.log_softmax(self.fc4(x),dim=1)
+    return x
+
+model = Net()
+transform = transforms.Compose([transforms.ToTensor(),
+                                transforms.Normalize((0.5),(0.5),)])
+trainset = datasets.MNIST('.data/', download=True, train=True, transform=transform)
+trainloader = torch.utils.data.DataLoader(trainset, batch_size=64, shuffle=True)
+testset = datasets.MNIST('.data/', download=True, train=False, transform=transform)
+testloader = torch.utils.data.DataLoader(testset, batch_size=64, shuffle=True)
+criterion = nn.NLLLoss()
+optimizer = optim.Adam(model.parameters(), lr=0.003)
+# learning rate at 0.03 is way too big for this, try 0.003
+
+epochs = 100
+steps = 0
+train_losses, test_losses = [], []
+for e in range(epochs):
+  running_loss = 0
+  for images, labels in trainloader:
+    optimizer.zero_grad()
+    log_ps = model(images)
+    loss = criterion(log_ps, labels)
+    loss.backward()
+    optimizer.step()
+    running_loss += loss.item()
+  else:
+    test_loss = 0
+    accuracy = 0
+    with torch.no_grad():
+      model.eval()
+      for images, labels in testloader:
+        log_ps = model(images)
+        test_loss += criterion(log_ps, labels)
+        ps = torch.exp(log_ps)
+        top_p, top_class = ps.topk(1, dim=1)
+        equals = top_class == labels.view(*top_class.shape)
+        accuracy += torch.mean(equals.type(torch.FloatTensor))
+    model.train()
+    train_losses.append(running_loss/len(trainloader))
+    test_losses.append(test_loss/len(testloader))
+    print("Epoch: {}/{}.. ".format(e+1, epochs),
+              "Training Loss: {:.3f}.. ".format(running_loss/len(trainloader)),
+              "Test Loss: {:.3f}.. ".format(test_loss/len(testloader)),
+              "Test Accuracy: {:.3f}".format(accuracy/len(testloader)))
+plt.plot(train_losses, label='Training loss')
+plt.plot(test_losses, label='Validation loss')
+plt.legend(frameon=False)
